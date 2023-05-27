ec90bad0ca91e8c747d85616e7c05b1e5c0b155c
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-27 08:25:56 +0100

    First Commit

---
+++ TheReckoning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheReckoning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
