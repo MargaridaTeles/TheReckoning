43b71b8e594f2583a69519bd59cb57f507657159
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-27 08:48:28 +0100

    Criei as classes Controller e View

---
+++ TheReckoning/Controller.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace TheReckoning
+{
+    public class Controller
+    {
+    }
+}
---
+++ TheReckoning/View.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace TheReckoning
+{
+    public class View
+    {
+        
+    }
+}
