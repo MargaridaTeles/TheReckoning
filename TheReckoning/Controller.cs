906e20c349a7df77570ed40991c689aae39ef33a
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-29 09:13:27 +0100

    Adicionei comentário com código de exemplo

--- TheReckoning/Controller.cs
+++ TheReckoning/Controller.cs
@@ -4,5 +4,44 @@ namespace TheReckoning
 {
     public class Controller
     {
+        private List<Carta> list;
+        /*public void Run(IView view)
+        {
+            int input;
+            this.view = view;
+            do
+            {
+                // 1 -> Insert player
+                // 2 -> List all players
+                // 3 -> List players w/ score > x
+                // 4 -> Change player sorting criteria
+                // 0 -> Exit
+                input = view.MainMenu(playerOrder);
+
+                switch (input)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        InsertPlayer();
+                        break;
+                    case 2:
+                        SortPlayers();
+                        view.ShowPlayers(list);
+                        break;
+                    case 3:
+                        SortPlayers();
+                        ShowPlayersWithScore();
+                        break;
+                    case 4:
+                        ChangePlayerOrder();
+                        break;
+                    default:
+                        view.InvalidOption();
+                        break;
+                }
+            }
+            while (input != 0);
+        }*/
     }
 }
