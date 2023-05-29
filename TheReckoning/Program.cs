bb7fe3ac28cda6001cebf2b9015972711b63d7cf
MargaridaTeles <telesmaegarida@gmail.com>
2023-05-29 09:14:05 +0100

    Adicionei cartas como exemplo

--- TheReckoning/Program.cs
+++ TheReckoning/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace TheReckoning
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            
+            // List of cartas is our model
+            List<Carta> list = new List<Carta>()
+            {
+                new Carta("Pedro", 50, 25, 26),
+                new Carta("Verde", 42, 50, 20),
+            };
+
+            // Create controller
+            //Controller controller = new Controller(list);
+
+            // Create view
+            //IView view = new UglyView(controller);
+
+            // Start program
+            //controller.Run(view);
         }
     }
 }
